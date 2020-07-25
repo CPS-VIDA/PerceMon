74914cc86627d001818aa5fce8c5b74bac55ef78
Anand Balakrishnan <anandbala1597@gmail.com>
2020-07-24 19:47:06 -0700

    Attempt to fix horizon

--- include/percemon/utils.hpp
+++ include/percemon/utils.hpp
@@ -10,6 +10,14 @@ namespace percemon {
 namespace utils {
 
 /**
+ * Helper for SNIFAE: checks if given variable is any one of the Args
+ */
+template <typename T, typename... Args>
+struct is_one_of : std::disjunction<std::is_same<std::decay_t<T>, Args>...> {};
+template <typename T, typename... Args>
+inline constexpr bool is_one_of_v = is_one_of<T, Args...>::value;
+
+/**
  * Visit helper for a set of visitor lambdas.
  *
  * @see https://en.cppreference.com/w/cpp/utility/variant/visit
--- src/monitoring/horizon.cc
+++ src/monitoring/horizon.cc
@@ -3,6 +3,10 @@
 
 #include <optional>
 #include <variant>
+#include <algorithm>
+#include <numeric>
+
+#include "percemon/utils.hpp"
 
 namespace percemon::monitoring {
 
@@ -14,14 +18,46 @@ struct HorizonCompute {
   HorizonCompute() = default;
   HorizonCompute(double fps_) : fps{fps_} {};
 
-  size_t operator()(Const) {
+  template <typename T>
+  size_t compute(const T&) {
     return 0;
   };
-  size_t operator(const TimeBound& e) {
+
+  template <>
+  size_t compute<TimeBound>(const TimeBound& e) {
+    if (!fps.has_value()) {
+      throw std::invalid_argument(
+          "Trying to get the horizon (in number of frames) with a TimeBound in the formula. No FPS provided.");
+    }
+    const double fps_val        = *fps;
+    const double frame_duration = 1 / fps_val;
+    double bound                = e.bound;
+    if (e.op == ComparisonOp::GT) { // Remove 1 frame
+      bound += frame_duration;
+    }
+    // TODO(anand): Verify this correctness.
+    return static_cast<size_t>(bound * fps_val);
   };
 
-  size_t eval(const Expr& expr) {
-    return std::visit(*this, expr);
+  template <>
+  size_t compute<FrameBound>(const FrameBound& e) {
+    if (e.op == ComparisonOp::GT) {
+      return e.bound - 1;
+    }
+    return e.bound;
+  };
+
+  template <typename T, std::enable_if_t<utils::is_one_of_v<T, AndPtr, OrPtr>>>
+      size_t compute(const T& e){
+        std::vector<size_t> sub_horizons;
+        std::transform<
+        return std::max(
+      }
+
+      size_t eval(const Expr& expr) {
+    return std::visit(
+        [&](const auto e) { return this->compute<std::decay_t<decltype(e)>>(e); },
+        expr);
   };
 };
 } // namespace
